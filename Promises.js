29712ab6dda3759e3d5744a88059fc109dd9d369
Oliver Tanner <oliver-tanner@outlook.com>
2018-08-10 23:26:53 +0200

    Add Error Handling With Catch

--- Promises.js
+++ Promises.js
@@ -14,4 +14,19 @@ function basicPromise (willResolve) {
 basicPromise(true)
     .then((promiseResult) => {
         console.log(promiseResult)
+    });
+
+function errorHandling () {
+    return new Promise ((resolve, reject) => {
+        throw new Error('Uh oh! Error!');
+        resolve('This never gets called');
+    });
+};
+
+errorHandling()
+    .then((promiseResult) => {
+        console.log(promiseResult);
+    })
+    .catch((error) => {
+        console.log(error)
     })
