b6f5bb3d29c8c3df0d11718a6d3689f5c17e0abd
Oliver Tanner <oliver-tanner@outlook.com>
2019-02-22 12:33:46 +0100

    Add example for bind with optional args

--- Functions/bind.js
+++ Functions/bind.js
@@ -15,3 +15,27 @@ let myBoundFunction = getA.bind(myObject);
 
 console.log("The unbound getA: ", getA());
 console.log("The getA bound to myObject: ", myBoundFunction());
+
+
+/*
+You can also pass optional arguments which will
+be applied to the function.
+*/
+
+let mySecondObject = {
+    a: 10
+}
+
+function addNumbers (...numbers) {
+    return numbers.reduce(
+        (prev, curr) => prev + curr,
+        parseInt(this.a)
+        );
+}
+
+let add10 = addNumbers.bind(mySecondObject);
+
+let add20 = addNumbers.bind(mySecondObject, 10);
+
+console.log("Add 10 to 1: ", add10(1));
+console.log("Add 20 to 1: ", add20(1));
