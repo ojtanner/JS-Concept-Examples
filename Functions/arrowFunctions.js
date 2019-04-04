2bdb3d1669783742954b4d6e86051dbf49cb83e9
Oliver Tanner <oliver-tanner@outlook.com>
2019-04-04 14:11:34 +0200

    Add eslint config and TODOs for arrowFunctions

--- Functions/arrowFunctions.js
+++ Functions/arrowFunctions.js
@@ -8,14 +8,14 @@ Section 1: Syntax
 ===================================================
 */
 
-console.log(`\nSection 1:\n`);
+console.log('\nSection 1:\n');
 
 let myFunction;
 
 //1.1) Standard function w/o return statement
 
 myFunction = function (/* parameters */) {
-    /* 
+	/* 
     function-body
 
     if you omit the return statement
@@ -25,24 +25,24 @@ myFunction = function (/* parameters */) {
     */
 };
 
-console.log("1.1) Standard function w/o return statement:", myFunction());
+console.log('1.1) Standard function w/o return statement:', myFunction());
 
 //1.2) Standard function
 
 myFunction = function (/* parameters */) {
-    /*
+	/*
     function-body
     */
 
-    return "explicit return value";
+	return 'explicit return value';
 };
 
-console.log("1.2) Standard function:", myFunction());
+console.log('1.2) Standard function:', myFunction());
 
 //1.3) Arrow function general form
 
 myFunction = (/* parameters */) => {
-    /*
+	/*
     multi-line function-body
 
     If you have a multi-line function-body
@@ -50,15 +50,15 @@ myFunction = (/* parameters */) => {
     your return value has to be set explicitly.
     */
 
-    return "arrow function explicit return value";
-}
+	return 'arrow function explicit return value';
+};
 
-console.log("1.3) Arrow function general form:", myFunction());
+console.log('1.3) Arrow function general form:', myFunction());
 
 //1.4) Arrow function short form
 
-myFunction = (/* parameters */) => "one-line implicit return value";
-    /* 
+myFunction = (/* parameters */) => 'one-line implicit return value';
+/* 
     single-line function-body
 
     If you have a single-line function-body
@@ -68,12 +68,12 @@ myFunction = (/* parameters */) => "one-line implicit return value";
     the single-line function-body.
     */
 
-console.log("1.4) Arrow function short form:", myFunction());
+console.log('1.4) Arrow function short form:', myFunction());
 
 //1.5) Arrow function single parameter form
 
 myFunction = parameter => {
-    /*
+	/*
     single-parameter form
 
     If your arrow function only has one
@@ -84,10 +84,10 @@ myFunction = parameter => {
     one-line function-body.
     */
 
-    return parameter;
+	return parameter;
 };
 
-console.log("1.5) Arrow function single parameter form:", myFunction("single parameter"));
+console.log('1.5) Arrow function single parameter form:', myFunction('single parameter'));
 
 /*
 ===================================================
@@ -95,31 +95,31 @@ Section 2: Execution context and "this"
 ===================================================
 */
 
-console.log(`\nSection 2:\n`);
+console.log('\nSection 2:\n');
 
 let myObject = {
-    value: "object value"
+	value: 'object value'
 };
 
 //2.1) Standard functions
 
 myFunction = function () {
-    /*
+	/*
     Standard functions get their execution context
     from the calling object.
 
     This function gets called with "this" set
     to myObject.
     */
-    return this.value;
+	return this.value;
 };
 
-console.log("2.1) Standard functions:",myFunction.call(myObject));
+console.log('2.1) Standard functions:',myFunction.call(myObject));
 
 //2.2) Arrow functions
 
 myFunction = () => {
-    /*
+	/*
     Arrow functions do NOT get their execution
     context from the calling object.
 
@@ -131,35 +131,35 @@ myFunction = () => {
     Even if we call it with .call(myObject),
     "this" is already set to the global object.
     */
-    return this.value;
+	return this.value;
 };
 
-console.log("2.2) Arrow functions:",myFunction.call(myObject));
+console.log('2.2) Arrow functions:',myFunction.call(myObject));
 
 //2.3) Standard functions as methods
 
 myObject = {
-    value: "object value",
-    myMethod: function () {
-        /*
+	value: 'object value',
+	myMethod: function () {
+		/*
         We define a method for myObject.
         The execution context and lexical scope
         of this method will always(*1) set "this"
         to the object that calls the method.
         In this case, myObject.
         */
-        return this.value;
-    }
+		return this.value;
+	}
 };
 
-console.log("2.3) Standard functions as methods:",myObject.myMethod());
+console.log('2.3) Standard functions as methods:',myObject.myMethod());
 
 //2.4) Arrow functions as methods
 
 myObject = {
-    value: "object value",
-    myMethod: () => {
-        /*
+	value: 'object value',
+	myMethod: () => {
+		/*
         This does not work!
 
         The lexical scope here is still the 
@@ -167,37 +167,37 @@ myObject = {
         function is still bound to the global 
         object and not myObject.
         */
-        return this.value;
-    }
+		return this.value;
+	}
 };
 
-console.log("2.4) Arrow functions as methods:",myObject.myMethod());
+console.log('2.4) Arrow functions as methods:',myObject.myMethod());
 
 //2.5) Arrow functions and bind
 
 myObject = {
-    value: "object value",
-    myMethod: (() => {
-        /*
+	value: 'object value',
+	myMethod: (() => {
+		/*
         This does not work!
 
         Arrow functions can not be
         re-bound.
         */
-        return this.value;
-    }).bind(myObject)
+		return this.value;
+	}).bind(myObject)
 };
 
-console.log("2.5) Arrow functions and bind:",myObject.myMethod());
+console.log('2.5) Arrow functions and bind:',myObject.myMethod());
 
 //2.6) Arrow functions inside standard functions
 
 myObject = {
-    value: "object value",
-    myMethod: function() {
-        let myValue;
-        (() => {
-            /*
+	value: 'object value',
+	myMethod: function() {
+		let myValue;
+		(() => {
+			/*
             Here the lexical scope of the arrow function
             is the outer function. 
             Since the outer function is a standard function
@@ -208,10 +208,22 @@ myObject = {
             We define a funcion expression and inmediatly invoke it.
             Also known as the good old IIFE.
             */
-            myValue = this.value;
-        })()
-        return myValue;
-    }
+			myValue = this.value;
+		})();
+		return myValue;
+	}
 };
 
-console.log("2.6) Arrow functions inside standard functions:", myObject.myMethod());
+console.log('2.6) Arrow functions inside standard functions:', myObject.myMethod());
+
+/*
+===================================================
+Section 3: Misc
+===================================================
+*/
+
+//TODO:
+
+//Arrow functions do not work as constructors and "new"
+
+//Arrow functions can not be set to an object via .prototype
