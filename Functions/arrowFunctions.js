faa0a8c6f33c4668136e9236baedff9bd57f8e07
Oliver Tanner <oliver-tanner@outlook.com>
2019-04-04 11:28:29 +0200

    Change arrow function examples and document structure

--- Functions/arrowFunctions.js
+++ Functions/arrowFunctions.js
@@ -1,81 +1,191 @@
-/*
-Arrow functions vs normal functions
+/**
+* Arrow functions
 */
 
-let normalFunction = function() {
-    //I am a normal function
+/*
+===================================================
+Section 1: Syntax
+===================================================
+*/
+
+console.log(`\nSection 1:\n`);
+
+let myFunction;
+
+//1.1) Standard function w/o return statement
+
+myFunction = function (/* parameters */) {
+    /* 
+    function-body
+
+    if you omit the return statement
+    in a standard function,
+    the return value will be of type
+    "undefined".
+    */
+};
+
+console.log("1.1) Standard function w/o return statement:", myFunction());
+
+//1.2) Standard function
+
+myFunction = function (/* parameters */) {
+    /*
+    function-body
+    */
+
+    return "explicit return value";
+};
+
+console.log("1.2) Standard function:", myFunction());
+
+//1.3) Arrow function general form
+
+myFunction = (/* parameters */) => {
+    /*
+    multi-line function-body
+
+    If you have a multi-line function-body
+    in your arrow function,
+    your return value has to be set explicitly.
+    */
+
+    return "arrow function explicit return value";
 }
 
-let arrowFunction = () => {
-    // I am an arrow funciton
-}
+console.log("1.3) Arrow function general form:", myFunction());
+
+//1.4) Arrow function short form
+
+myFunction = (/* parameters */) => "one-line implicit return value";
+    /* 
+    single-line function-body
+
+    If you have a single-line function-body
+    in your arrow function,
+    the return value will be implicitly set 
+    to the expression contained on 
+    the single-line function-body.
+    */
+
+console.log("1.4) Arrow function short form:", myFunction());
+
+//1.5) Arrow function single parameter form
+
+myFunction = parameter => {
+    /*
+    single-parameter form
+
+    If your arrow function only has one
+    parameter, you can omit the 
+    round brackets.
+
+    This form is also compatible with the 
+    one-line function-body.
+    */
+
+    return parameter;
+};
+
+console.log("1.5) Arrow function single parameter form:", myFunction("single parameter"));
 
 /*
-Lexical scope of arrow functions:
-
-Arrow functions inherit the value of this
-from the scope enclosing them.
-
-This means that the context is preserved.
+===================================================
+Section 2: Execution context and "this"
+===================================================
 */
 
-/*
-Normal nested function creates new lexical scope.
-"this" does not point to objec anymore.
+console.log(`\nSection 2:\n`);
 
-Bound nested function has context bound to myObject.
-Context is bound to "this" of object.
-
-Arrow function does not create new context for "this".
-"this" is inherited from enclosing function.
-Enclosing function is method of myObject,
-therefore "this" points to myObject.
-*/
 let myObject = {
-    a: "value of myObject.a",
-    self: this,
-    getA: function() {
-        return (function () {
-            //console.log("\"this\" in nested function points to: ", this);
-            return this.a;
-        })();
-    },
-    getBoundA: function() {
-        return (function () {
-            //console.log("\"this\" in nested function points to: ", this);
-            return this.a
-        }.bind(myObject))();
-    },
-    getArrowA: function() {
-        return (() => {
-            //console.log("\"this\" in nested function points to: ", this);
-            return this.a
-        })();
+    value: "object value"
+};
+
+//2.1) Standard functions
+
+myFunction = function () {
+    /*
+    Standard functions get their execution context
+    from the calling object.
+
+    This function gets called with "this" set
+    to myObject.
+    */
+    return this.value;
+};
+
+console.log("2.1) Standard functions:",myFunction.call(myObject));
+
+//2.2) Arrow functions
+
+myFunction = () => {
+    /*
+    Arrow functions do NOT get their execution
+    context from the calling object.
+
+    Rather, they get the execution context from
+    the lexical scope they where written in.
+
+    Here, the arrow function is written in the
+    lexical scope of the global object.
+    Even if we call it with .call(myObject),
+    "this" is already set to the global object.
+    */
+    return this.value;
+};
+
+console.log("2.2) Arrow functions:",myFunction.call(myObject));
+
+//2.3) Standard functions as methods
+
+myObject = {
+    value: "object value",
+    myMethod: function () {
+        /*
+        We define a method for myObject.
+        The execution context and lexical scope
+        of this method will always(*1) set "this"
+        to the object that calls the method.
+        In this case, myObject.
+        */
+        return this.value;
     }
-}
+};
 
-/*
-Arrow function as direct method does inherit context from enclosing
-lexical scope, which as of this point is NOT mySecondObject.
-*/
-let mySecondObject = {
-    a: "value of mySecondObject.a",
-    getA: () => {
-        console.log("\"this\" in nested function points to: ", this);
-        return this.a;
+console.log("2.3) Standard functions as methods:",myObject.myMethod());
+
+//2.4) Arrow functions as methods
+
+myObject = {
+    value: "object value",
+    myMethod: () => {
+        /*
+        This does not work!
+
+        The lexical scope here is still the 
+        global object! Therefore, the arrow
+        function is still bound to the global 
+        object and not myObject.
+        */
+        return this.value;
     }
-}
+};
 
-function printA () {
-    console.log("Normal function nested in method: ",myObject.getA() ,"\n");
-    console.log("Normal function nexted bound to object",myObject.getBoundA() ,"\n");
-    console.log("Arrow funciton nested in method: ",myObject.getArrowA() ,"\n");
-}
+console.log("2.4) Arrow functions as methods:",myObject.myMethod());
 
-function printSecondA () {
-    console.log("Arrow function as method without another enclosing method: ",mySecondObject.getA() ,"\n");
-}
+//2.5) Arrow functions and bind
 
-printA();
-console.log("\n");
-printSecondA();
+myObject = {
+    value: "object value",
+    myMethod: (() => {
+        /*
+        This does not work!
+
+        Arrow functions can not be
+        re-bound.
+        */
+        return this.value;
+    }).bind(myObject)
+};
+
+console.log("2.5) Arrow functions and bind:",myObject.myMethod());
