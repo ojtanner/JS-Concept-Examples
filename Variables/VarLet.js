16eb278ae04d354d824ff06540dfda9c861d730f
tanneoli <oliver-tanner@outlook.com>
2021-11-27 16:27:47 +0100

    Update VarLet

--- Variables/VarLet.js
+++ Variables/VarLet.js
@@ -2,30 +2,79 @@
 
 /**
  * Var vs Let
- * 
+ */
+
+/*
+===================================================
+Section 1: Scope
+===================================================
+*/
+
+console.log("1.) Scope");
+
+/*
  * Var is function scoped
  * Let is block scoped
  */
+function hereBeVar() {
+  for (var i = 0; i < 4; i++) {
+    console.log("Var inside the loop and inside function-scope: ", i);
+  }
+  console.log("Var outside loop and inside function-scope: ", i);
+}
 
-function hereBeVar () {
-    for(var i = 0; i < 4; i++) {
-        console.log('Var inside the loop and inside function-scope: ', i);
-    };
-    console.log('Var outside loop and inside function-scope: ', i);
- };
-
-function hereBeLet () {
-    console.log('\n')
-    for(let i = 0; i < 4; i++) {
-        console.log('Let inside the loop and inside function-scope: ', i);
-    };
-    try {
-        console.log('Let outside loop and inside function-scope: ', i);
-    }
-    catch (error) {
-        console.log('Error: seems like i is not defined outside the containing block\n', error)
-    }
- };
+function hereBeLet() {
+  console.log("\n");
+  for (let i = 0; i < 4; i++) {
+    console.log("Let inside the loop and inside function-scope: ", i);
+  }
+  try {
+    console.log("Let outside loop and inside function-scope: ", i);
+  } catch (error) {
+    console.log(
+      "Error: seems like i is not defined outside the containing block\n",
+      error
+    );
+  }
+}
 
 hereBeVar();
 hereBeLet();
+
+/*
+===================================================
+Section 2: Re-declaration and reassignment
+===================================================
+*/
+
+console.log("2.) Re-declaration and reassignment");
+
+/*
+ * Var can be redeclared as well as reassigned.
+ */
+
+var myName = "Fred";
+console.log(`Declared var myName: ${myName}`);
+
+var myName = "Barney";
+console.log(`Redeclared var myName: ${myName}`);
+
+myName = "Wilma";
+console.log(`Reassigned var myName: ${myName}`);
+
+/*
+ * Let can not be redeclared, but can be reassigned.
+ */
+
+let mySurname = "Flintstone";
+console.log(`Declared let mySurname ${mySurname}`);
+
+try {
+  let mySurname = "Rubble";
+} catch (error) {
+  console.log('Uh-oh, you can\'t redeclare let!');
+  console.error("\n", error);
+}
+
+mySurname = "Rubble";
+console.log(`Reassigned let mySurname ${mySurname}`);
